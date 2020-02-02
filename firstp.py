2419812d97c83ccec805c9c441dad1ab761fcfed
alexanderzou <azou00@stuy.edu>
2020-02-01 22:01:37 -0500

    some pic changes

--- firstp.py
+++ firstp.py
@@ -15,21 +15,21 @@ def main(x, y, filename):
                 g = 255
             elif i > x*y and i < 2*x*y/3:
                 r = 255
-                g = 0
+                g = 255
                 b = 255
             else:
                 r = 255
                 g = 255
-                g = 255
+                b = 0
         else:
             if i < x*y/3:
                 r = random.randint(0,255)
                 g = int(float(i) / float(x*y) * 255)
                 b = int(255 - (float(i) / float(x*y)) * 255)
             elif i > x*y/3 and i < 2*x*y/3:
-                r = int(255 - (float(i) / float(x*y)) * 255)
-                g = random.randint(0,255)
-                b = int(float(i) / float(x*y) * 255)
+                r = 0
+                g = 0
+                b = 0
             else:
                 r = int(float(i) / float(x*y) * 255)
                 g = int(255 - (float(i) / float(x*y)) * 255)
